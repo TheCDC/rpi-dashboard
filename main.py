a42ab3cd0f2ef359c8dc15adb6e04413fc8ebff0
Christopher Chen <christopher.chen1995@gmail.com>
2017-04-27 09:44:32 -0700

    revert resolution

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import sys
 def main():
     pygame.init()
     infoObj = pygame.display.Info()
-    # WIDTH = int(infoObj.current_w)
-    # HEIGHT = int(infoObj.current_h)
-    WIDTH = 800
-    HEIGHT = 480
+    WIDTH = int(infoObj.current_w)
+    HEIGHT = int(infoObj.current_h)
+    # WIDTH = 800
+    # HEIGHT = 480
     size = (WIDTH, HEIGHT)
     CLOCK = pygame.time.Clock()
 
@@ -28,7 +28,7 @@ def main():
                 pygame.mixer.quit()
                 pygame.quit()
                 sys.exit()
-        v = (frames / 60) * 255
+        v = (frames / 16) * 255
         DISPLAYSURF.fill((v, v, v))
         pygame.display.update()
         frames += 1
