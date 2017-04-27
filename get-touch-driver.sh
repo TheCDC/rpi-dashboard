d162df4535dc05ec43da2459136d13e4408fe38f
Christopher Chen <christopher.chen1995@gmail.com>
2017-04-27 14:29:28 -0700

    directory

--- get-touch-driver.sh
+++ get-touch-driver.sh
@@ -1,5 +1,5 @@
 wget http://www.waveshare.com/w/upload/4/4b/LCD-show-161112.tar.gz
-sudo tar xzvf /boot/LCD-show-*.tar.gz  -C /boot/
+sudo tar xzvf LCD-show-*.tar.gz  -C /boot/
 cd /boot/LCD-show/
 chmod +x LCD5-show
 ./LCD5-show
