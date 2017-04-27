c5776388b9086e4c73bd6841bf3e5a763ef9b711
Christopher Chen <christopher.chen1995@gmail.com>
2017-04-27 09:17:54 -0700

    dependency script

---
+++ pygame-dependencies.sh
@@ -0,0 +1,3 @@
+sudo apt install python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev \
+  libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev \
+  libavformat-dev libswscale-dev libjpeg-dev libfreetype6-dev
